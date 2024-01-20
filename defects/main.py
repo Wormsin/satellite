6bb498221893035f1ffbdcb0d25b8a3b7353a7b5
wormsin <bobdomeme@gmail.com>
2024-01-20 17:08:40 +0300

    fix

--- defects/main.py
+++ defects/main.py
@@ -3,16 +3,21 @@ from shutil import copy, rmtree
 import cv2
 import numpy as np
 from lines import add_lines
+import albumentations as A
 
 def save_data(num, image, bbox_arr, cls):
     name = str(format((num+1)/1000000, '6f'))
     img_path = os.path.join(IMG_PATH, f'{name[2:]}.jpg')
     bbox_path = os.path.join(LABELS_PATH, f'{name[2:]}.txt')
     cv2.imwrite(img_path, image)
-    for bbox in bbox_arr:
-        line = (cls, *bbox)
-        with open(bbox_path, 'a') as f:
-            f.write(('%g ' * len(line)).rstrip() % line + '\n')
+    if len(bbox_arr)!=0:
+        for bbox in bbox_arr:
+            line = (cls, *bbox)
+            with open(bbox_path, 'a') as f:
+                f.write(('%g ' * len(line)).rstrip() % line + '\n')
+    else:
+        file = open(bbox_path, 'a')
+        file.close()
 
 def randv_big_lines():
     noise = False
@@ -43,7 +48,7 @@ def randv_packages():
     brightness = int(np.random.uniform(0.5, 1)*100)
     frequency = np.random.uniform(0.01, 0.2)
     variance = np.random.uniform(0.3, 1)
-    gamma = np.random.uniform(0, 0.4)
+    gamma = np.random.uniform(0, 0.6)
     return (x, y), vertical, dark, brightness, line_amplitude, mask_width, frequency, gamma, variance, noise
 
 def randv_destroyed_pixels():
@@ -61,6 +66,22 @@ def randv_destroyed_pixels():
     gamma = np.random.uniform(0, 0.2)
     return (x, y), vertical, dark, brightness, line_amplitude, mask_width, frequency, gamma, variance, noise
 
+def augment(image, bboxes, cls):
+    class_labels = np.zeros(len(bboxes))
+    transform = A.Compose([
+        A.HorizontalFlip(p=0.5),
+        A.ShiftScaleRotate(p=0.5),
+        A.RandomBrightnessContrast(p=1),
+        A.RandomCrop(width=560, height=560, p=0.4),
+    ],
+    bbox_params=A.BboxParams(format='yolo', min_area=25, label_fields=['class_labels']),
+    )
+    transformed = transform(image=image, bboxes=bboxes, class_labels=class_labels)
+    transformed_image = transformed['image']
+    transformed_bboxes = transformed['bboxes']
+
+    return transformed_image, transformed_bboxes
+
 def make_bbox(x, y, mask_width, line_amplitude, vertical, variance, frequency, gamma):
     lambd = frequency*mask_width
     w = mask_width/2
@@ -76,11 +97,10 @@ def make_bbox(x, y, mask_width, line_amplitude, vertical, variance, frequency, g
         for i in range(n+1):
             cp = lambd*i+lambd/4
             if np.exp(-(cp**2)*0.5/(variance*w*2)**2) >= 0.45 and cp<mask_width/2:
-                #X_centers.append(xcp)
                 xb = (Xc+cp)*vertical+Xc*(not vertical)
                 yb = Yc*vertical+(Yc+cp)*(not vertical)
                 bbox.append([xb/100, yb/100, width/100, height/100])
-            cm = -lambd*i+lambd/4
+            cm = -lambd*(i+1)+lambd/4
             if np.exp(-(cm**2)*0.5/(variance*w*2)**2) >= 0.45 and -cm<mask_width/2:
                 xb = (Xc+cm)*vertical+Xc*(not vertical)
                 yb = Yc*vertical+(Yc+cm)*(not vertical)
@@ -91,40 +111,69 @@ def make_bbox(x, y, mask_width, line_amplitude, vertical, variance, frequency, g
         bbox.append([Xc/100, Yc/100, mask_width/100, line_amplitude/100])
     return bbox
 
+def process_img(image, defect_func, num, num_cycles_defects, augmentation, display):
+    bbox_arr = []
+    for j in range(num_cycles_defects):
+        location, vertical, dark, brightness, line_amplitude, mask_width, frequency, gamma, variance, noise = defect_func()
+        bbox_arr+= make_bbox(location[0], location[1], mask_width, line_amplitude, vertical, variance, frequency, gamma)
+        image = add_lines(image, location, vertical, dark, brightness, line_amplitude, mask_width, frequency, gamma, variance, noise)
+    if display: display_img(image, bbox_arr)
+    save_data(num, image, bbox_arr, 0)
+    for i in range(augmentation):
+        aug_img, aug_bboxes = augment(image, bboxes=bbox_arr, cls=0)
+        if display: display_img(aug_img, aug_bboxes)
+        save_data(num+1+i, aug_img, aug_bboxes, 0)
+    return augmentation
 
-IMG_PATH = 'dataset/images/train'
-LABELS_PATH = 'dataset/labels/train'
-SOURCE = "images"
-
-
-if os.path.exists(IMG_PATH):
-        rmtree("dataset")
-os.makedirs(IMG_PATH)
-os.makedirs(LABELS_PATH)
-
-for num, filename in enumerate(os.listdir(SOURCE)):
-    image = cv2.imread(os.path.join(SOURCE, filename))
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    if num <=5:
-        location, vertical, dark, brightness, line_amplitude, mask_width, frequency, gamma, variance, noise = randv_big_lines()
-    elif num >5 and num <=8:
-        location, vertical, dark, brightness, line_amplitude, mask_width, frequency, gamma, variance, noise = randv_packages()
-    else:
-        location, vertical, dark, brightness, line_amplitude, mask_width, frequency, gamma, variance, noise = randv_destroyed_pixels()
-
-    bbox_arr = make_bbox(location[0], location[1], mask_width, line_amplitude, vertical, variance, frequency, gamma)
-    defected_img = add_lines(gray, location, vertical, dark, brightness, line_amplitude, mask_width, frequency, gamma, variance, noise)
-    save_data(num, defected_img, bbox_arr, 0)
-
-    img = cv2.resize(defected_img, (960, 960)) 
+def display_img(image, bbox_arr):
+    img = cv2.resize(image, (960, 960)) 
     height, width = img.shape
     for bbox in bbox_arr:
         x_center = int(bbox[0]*width)
         y_center  = int(bbox[1]*height)
         m_width = int(bbox[2]*width)
         m_height = int(bbox[3]*height)
-        cv2.rectangle(img, (int(x_center-m_width/2), int(y_center-m_height/2)), (int(x_center+m_width/2), int(y_center+m_height/2)), color=(255,255,255), thickness=2)
+        cv2.rectangle(img, (int(x_center-m_width/2), int(y_center-m_height/2)), (int(x_center+m_width/2), int(y_center+m_height/2)), color=(255,255,255), thickness=2) 
     cv2.imshow("image", img)
     cv2.waitKey(0) 
     cv2.destroyAllWindows()
-    print(location, vertical, dark, brightness, line_amplitude, mask_width, frequency, gamma, variance, noise)
+
+IMG_PATH = 'dataset/images/train'
+LABELS_PATH = 'dataset/labels/train'
+SOURCE = "validation_data"
+
+def main():
+    name_shift = 0
+    if not os.path.exists(IMG_PATH):
+        os.makedirs(IMG_PATH)
+        os.makedirs(LABELS_PATH)
+    else:
+        filenames = os.listdir('dataset/labels/train')
+        if len(filenames)>0:
+            filenames = np.array([file.rstrip(".txt") for file in filenames])
+            name_shift = np.max(np.int32(filenames))
+
+    num_images = len(os.listdir(SOURCE))//3
+    for num, filename in enumerate(os.listdir(SOURCE)):
+        num+=name_shift
+        print(num-name_shift)
+        image = cv2.imread(os.path.join(SOURCE, filename))
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        if gray.shape[0] > 3000:
+            gray = cv2.resize(gray, (1080, 1080)) 
+
+        #name_shift += process_img(gray, randv_big_lines, num, 0, 2, True)
+        
+        if num<=num_images:
+            num_cycles_defects = np.random.randint(1, 3)
+            name_shift += process_img(gray, randv_big_lines, num, num_cycles_defects, 0, False)
+        elif num>num_images and num<=num_images*2:
+            num_cycles_defects = np.random.randint(1, 5)
+            name_shift += process_img(gray, randv_packages, num, num_cycles_defects, 0, False)
+        else:
+            num_cycles_defects = np.random.randint(1, 5)
+            name_shift += process_img(gray, randv_destroyed_pixels, num, num_cycles_defects, 0, False)
+
+
+if __name__ == "__main__":
+    main()
