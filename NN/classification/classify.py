5486df668460a6fae789b924aad53f43f78e5fe4
wormsin <bobdomeme@gmail.com>
2024-04-29 20:14:06 +0300

    add main condition

--- NN/classification/classify.py
+++ NN/classification/classify.py
@@ -3,25 +3,26 @@ import models as m
 import params
 import os
 
-args = params.parameters()
+if __name__ == "__main__":
+    args = params.parameters()
 
 
-dir = args.dataset
-model1 = args.weights[0]
-model2 =  args.weights[1]
-classes2 = args.classes
-device = args.device
+    dir = args.dataset
+    model1 = args.weights[0]
+    model2 =  args.weights[1]
+    classes2 = args.classes
+    device = args.device
 
-classes1 = ['defected', 'clear']
-name1 = args.name[0]
-name2 = args.name[1]
+    classes1 = ['defected', 'clear']
+    name1 = args.name[0]
+    name2 = args.name[1]
 
-model1, transform1 = m.model4classify(name=name1, classes=classes1, weights=model1, device=device)
-model2, transform2 = m.model4classify(name=name2, classes=classes2, weights=model2, device=device)
+    model1, transform1 = m.model4classify(name=name1, classes=classes1, weights=model1, device=device)
+    model2, transform2 = m.model4classify(name=name2, classes=classes2, weights=model2, device=device)
 
 
-utils.classification(dir = dir, transform = transform1, model= model1, classes= classes1, device=device)
-utils.classification(dir = classes1[0], transform = transform2, model= model2, classes= classes2, device=device)
+    utils.classification(dir = dir, transform = transform1, model= model1, classes= classes1, device=device)
+    utils.classification(dir = classes1[0], transform = transform2, model= model2, classes= classes2, device=device)
 
-os.rmdir(classes1[0])
-os.rmdir(classes1[1])
+    os.rmdir(classes1[0])
+    os.rmdir(classes1[1])
--- NN/classification/train.py
+++ NN/classification/train.py
@@ -2,20 +2,21 @@ import utils
 import models
 import params
 
-args = params.parameters()
+if __name__ == "__main__":
+    args = params.parameters()
 
-train_dir = args.dataset + '/train'
-test_dir =  args.dataset +'/test'
-name = args.name[0]
-batch_size = args.batch
-lr = args.lr
-epochs = args.epochs
+    train_dir = args.dataset + '/train'
+    test_dir =  args.dataset +'/test'
+    name = args.name[0]
+    batch_size = args.batch
+    lr = args.lr
+    epochs = args.epochs
 
-model, optimizer, loss_fn, device, train_loader, test_loader, classes = models.model4train(name=name, train_dir=train_dir, test_dir=test_dir, 
-                                                                                           batch_size=batch_size, lr=lr)
+    model, optimizer, loss_fn, device, train_loader, test_loader, classes = models.model4train(name=name, train_dir=train_dir, test_dir=test_dir, 
+                                                                                            batch_size=batch_size, lr=lr)
 
-print(f"classes: {classes}")
-print(f"device: {device}")
+    print(f"classes: {classes}")
+    print(f"device: {device}")
 
-utils.train(num_epochs=epochs, optimizer=optimizer, model=model, loss_fn=loss_fn, train_loader=train_loader, 
-            test_loader=test_loader, device=device, checkpoint=True)
+    utils.train(num_epochs=epochs, optimizer=optimizer, model=model, loss_fn=loss_fn, train_loader=train_loader, 
+                test_loader=test_loader, device=device, checkpoint=True)
